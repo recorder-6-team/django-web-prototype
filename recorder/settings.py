99fb26d26cff465478e90d1a89569c5300e9a058
John van Breda <john.vanbreda@biodiverseit.co.uk>
2021-06-28 15:34:48 +0100

    Changed API paging size

--- recorder/settings.py
+++ recorder/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 1000,
 }
 
 from recorder.local_settings import *
