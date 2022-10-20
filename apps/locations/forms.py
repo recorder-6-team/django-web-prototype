356897ae0a2fe32986d514749fa8089259cfc0c4
John van Breda <john.vanbreda@biodiverseit.co.uk>
2022-10-20 09:26:56 +0100

    Separate location form into General and OtherApproach
    Start of restructure into different form sections.
    Also added placeholders where relevant.

--- apps/locations/forms.py
+++ apps/locations/forms.py
@@ -1,15 +1,42 @@
 from django import forms
 from .models import Location, LocationName
+from django.utils.translation import gettext as _
 
 LocationNameFormSet = forms.inlineformset_factory(Location, LocationName, fields=('item_name','preferred',), extra=1)
 
-class LocationUpdateForm(forms.ModelForm):
+class LocationUpdateGeneralForm(forms.ModelForm):
   class Meta:
     model = Location
     fields = [
-      'description',
-      'spatial_ref',
       'file_code',
+      'location_type_key',
+      'spatial_ref',
+      'description',
+    ]
+    widgets = {
+      'file_code': forms.TextInput(
+        attrs = {
+          'placeholder': _('Any code used to identify the location can be entered here')
+        }
+      )
+    }
+
+class LocationUpdateOtherApproachForm(forms.ModelForm):
+  class Meta:
+    model = Location
+    fields = [
       'approach',
       'restriction',
     ]
+    widgets = {
+      'approach': forms.Textarea(
+        attrs = {
+          'placeholder': _('Enter details of getting to the location')
+        }
+      ),
+      'restriction': forms.Textarea(
+        attrs = {
+          'placeholder': _('Details of any access restrictions')
+        }
+      )
+    }
--- apps/locations/urls.py
+++ apps/locations/urls.py
@@ -2,11 +2,18 @@ from django.urls import path, include
 
 from .views import LocationDetailView
 from .views import LocationListView
-from .views import LocationUpdateView
+from .views import LocationUpdateGeneralView
+from .views import LocationUpdateOtherApproachView
 
 app_name = 'locations'
 urlpatterns = [
+    # Locations main page.
     path('', LocationListView.as_view(), name='index'),
-    path('<str:pk>/update', LocationUpdateView.as_view(), name='update'),
+    # Details view, normally loaded into main page via AJAX.
     path('<str:pk>/', LocationDetailView.as_view(), name='view'),
+    # Individual section forms, loaded via AJAX.
+    path('<str:pk>/update-general', LocationUpdateGeneralView.as_view(), name='updateGeneral'),
+    path('<str:pk>/update-other-approach', LocationUpdateOtherApproachView.as_view(), name='updateOtherApproach'),
+
+
 ]
