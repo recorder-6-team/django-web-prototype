59de96aa0f098e89a301e5388f74f1a87f5dc381
John van Breda <john.vanbreda@biodiverseit.co.uk>
2022-10-20 09:24:39 +0100

    Adds a filter for location non-preferred names

--- apps/glue/templatetags/recorder_filters.py
+++ apps/glue/templatetags/recorder_filters.py
@@ -4,6 +4,7 @@ from striprtf.striprtf import rtf_to_text
 
 register = template.Library()
 
+# A filter to retrieve the location preferred name.
 @register.filter(is_safe=True)
 def preferred_name(value):
   for name in value.names.all():
@@ -11,13 +12,20 @@ def preferred_name(value):
       return name.item_name
   return 'Unknown'
 
-# TODO check if striprtf is still necessary, as handled within models?
+# A filter to retrieve a list of alternative location names.
+@register.filter(is_safe=True)
+def alternative_names(value):
+  return value.names.filter(preferred=0)
 
+# A filter to strip RTF from text in a Recorder 6 database field.
+# TODO check if striprtf is still necessary, as handled within models?
+# TODO could we convert RTF to HTML?
 @register.filter(is_safe=True)
 @stringfilter
 def striprtf(value):
   return rtf_to_text(value).strip()
 
+# A filter to convert new lines in text into HTML <br> elements.
 @register.filter(is_safe=True)
 @stringfilter
 def newlinetohtml(value):
