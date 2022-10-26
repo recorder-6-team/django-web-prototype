3390f27499afa64058254088cc9a5834c1a3a35e
John van Breda <john.vanbreda@biodiverseit.co.uk>
2022-10-26 15:10:29 +0100

    More class renaming for consistency

--- apps/locations/urls.py
+++ apps/locations/urls.py
@@ -4,7 +4,7 @@ from .views import location
 from .views import names
 from .views import general
 from .views import designations
-from .views import other_approach
+from .views import other__approach
 
 app_name = 'locations'
 urlpatterns = [
@@ -17,10 +17,10 @@ urlpatterns = [
   path('<str:pk>/view/names', names.LocationDetailNamesView.as_view(), name='view-names'),
   path('<str:pk>/view/general', general.LocationDetailGeneralView.as_view(), name='view-general'),
   path('<str:pk>/view/designations', designations.LocationDetailDesignationsView.as_view(), name='view-designations'),
-  path('<str:pk>/view/other--approach', other_approach.LocationDetailOtherApproachView.as_view(), name='view-other--approach'),
+  path('<str:pk>/view/other--approach', other__approach.LocationDetailOtherApproachView.as_view(), name='view-other--approach'),
   # Individual section forms, loaded via AJAX.
   path('<str:pk>/update/names', names.LocationUpdateNamesView.as_view(), name='update-names'),
   path('<str:pk>/update/general', general.LocationUpdateGeneralView.as_view(), name='update-general'),
   path('<str:pk>/update/designations', designations.LocationUpdateDesignationsView.as_view(), name='update-designations'),
-  path('<str:pk>/update/other--approach', other_approach.LocationUpdateOtherApproachView.as_view(), name='update-other--approach'),
+  path('<str:pk>/update/other--approach', other__approach.LocationUpdateOtherApproachView.as_view(), name='update-other--approach'),
 ]
