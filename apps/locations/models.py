bbb8fa70c4ff6c03a857ef411b9b5413279c71a8
John van Breda <john.vanbreda@biodiverseit.co.uk>
2022-10-26 14:31:22 +0100

    Migrations refreshed

--- apps/locations/models.py
+++ apps/locations/models.py
@@ -6,6 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from apps.glue import FixedCharField
 from apps.glue import RtfTextField
 from apps.names.models import Name
 
