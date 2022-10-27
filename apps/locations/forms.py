fa4a6da24b89d598ad797b3fedf41fb97c286aef
John van Breda <john.vanbreda@biodiverseit.co.uk>
2022-10-27 16:37:21 +0100

    Inline form tidying

--- apps/locations/forms.py
+++ apps/locations/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from .models import Location, LocationName, LocationDesignation
 from django.utils.translation import gettext as _
+from .models import Location, LocationName, LocationDesignation
+from django_select2 import forms as s2forms
 
 LocationNameFormSet = forms.inlineformset_factory(Location, LocationName, fields=('item_name','preferred',), extra=1)
 
@@ -8,6 +9,7 @@ class LocationUpdateNamesForm(forms.ModelForm):
   class Meta:
     model = Location
     fields = [ ]
+
 
 class LocationUpdateGeneralForm(forms.ModelForm):
   class Meta:
@@ -23,15 +25,35 @@ class LocationUpdateGeneralForm(forms.ModelForm):
         attrs = {
           'placeholder': _('Any code used to identify the location can be entered here')
         }
-      )
+      ),
+    }
+    labels = {
+      'location_type_key': _('Location type'),
     }
 
-LocationDesignationFormSet = forms.inlineformset_factory(Location, LocationDesignation, fields=('site_status_key','ref_code','authority','date_from','date_to','comment'), extra=1)
+
+LocationDesignationFormSet = forms.inlineformset_factory(
+    Location,
+    LocationDesignation,
+    fields=(
+      'site_status_key',
+      'ref_code',
+      'authority',
+      'date_from',
+      'date_to',
+      'comment',
+    ),
+    labels = {
+      'site_status_key': _('Site status'),
+    },
+    extra=1,
+  )
 
 class LocationUpdateDesignationsForm(forms.ModelForm):
   class Meta:
     model = Location
     fields = [ ]
+
 
 class LocationUpdateOtherApproachForm(forms.ModelForm):
   class Meta:
@@ -43,7 +65,7 @@ class LocationUpdateOtherApproachForm(forms.ModelForm):
     widgets = {
       'approach': forms.Textarea(
         attrs = {
-          'placeholder': _('Enter details of getting to the location')
+          'placeholder': _('Describe how to get to the location')
         }
       ),
       'restriction': forms.Textarea(
