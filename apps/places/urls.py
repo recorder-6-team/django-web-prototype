6790027bf52bf7be70a800db2e3545a252588136
John van Breda <john.vanbreda@biodiverseit.co.uk>
2022-11-23 11:14:39 +0000

    Paths corrected to match section machine names

--- apps/places/urls.py
+++ apps/places/urls.py
@@ -12,6 +12,7 @@ from .views import location__other__tenures
 from .views import location__other__approach
 from .views import location_feature
 from .views import location_feature__general
+from .views import location_feature__damage_occurrences
 
 app_name = 'places'
 urlpatterns = [
@@ -25,22 +26,22 @@ urlpatterns = [
   path('location/<str:pk>/view/names', location__names.LocationDetailNamesView.as_view(), name='view--location--names'),
   path('location/<str:pk>/view/general', location__general.LocationDetailGeneralView.as_view(), name='view--location--general'),
   path('location/<str:pk>/view/designations', location__designations.LocationDetailDesignationsView.as_view(), name='view--location--designations'),
-  path('location/<str:pk>/view/geo-info/admin-areas', location__geo_info__admin_areas.LocationDetailGeoInfoAdminAreasView.as_view(), name='view--location--geo-info--admin-areas'),
-  path('location/<str:pk>/view/geo-info/land-parcels', location__geo_info__land_parcels.LocationDetailGeoInfoLandParcelsView.as_view(), name='view--location--geo-info--land-parcels'),
-  path('location/<str:pk>/view/other/relations', location__other__relations.LocationDetailOtherRelationsView.as_view(), name='view--location--other--relations'),
-  path('location/<str:pk>/view/other/uses', location__other__uses.LocationDetailOtherUsesView.as_view(), name='view--location--other--uses'),
-  path('location/<str:pk>/view/other/tenures', location__other__tenures.LocationDetailOtherTenuresView.as_view(), name='view--location--other--tenures'),
-  path('location/<str:pk>/view/other/approach', location__other__approach.LocationDetailOtherApproachView.as_view(), name='view--location--other--approach'),
+  path('location/<str:pk>/view/geo-info--admin-areas', location__geo_info__admin_areas.LocationDetailGeoInfoAdminAreasView.as_view(), name='view--location--geo-info--admin-areas'),
+  path('location/<str:pk>/view/geo-info--land-parcels', location__geo_info__land_parcels.LocationDetailGeoInfoLandParcelsView.as_view(), name='view--location--geo-info--land-parcels'),
+  path('location/<str:pk>/view/other--relations', location__other__relations.LocationDetailOtherRelationsView.as_view(), name='view--location--other--relations'),
+  path('location/<str:pk>/view/other--uses', location__other__uses.LocationDetailOtherUsesView.as_view(), name='view--location--other--uses'),
+  path('location/<str:pk>/view/other--tenures', location__other__tenures.LocationDetailOtherTenuresView.as_view(), name='view--location--other--tenures'),
+  path('location/<str:pk>/view/other--approach', location__other__approach.LocationDetailOtherApproachView.as_view(), name='view--location--other--approach'),
   # Individual section forms, loaded via AJAX.
   path('location/<str:pk>/update/names', location__names.LocationUpdateNamesView.as_view(), name='update--location--names'),
   path('location/<str:pk>/update/general', location__general.LocationUpdateGeneralView.as_view(), name='update--location--general'),
   path('location/<str:pk>/update/designations', location__designations.LocationUpdateDesignationsView.as_view(), name='update--location--designations'),
-  path('location/<str:pk>/update/geo-info/admin-areas', location__geo_info__admin_areas.LocationUpdateGeoInfoAdminAreasView.as_view(), name='update--location--geo-info--admin-areas'),
-  path('location/<str:pk>/update/geo-info/land-parcels', location__geo_info__land_parcels.LocationUpdateGeoInfoLandParcelsView.as_view(), name='update--location--geo-info--land-parcels'),
-  path('location/<str:pk>/update/other/relations', location__other__relations.LocationUpdateOtherRelationsView.as_view(), name='update--location--other--relations'),
-  path('location/<str:pk>/update/other/uses', location__other__uses.LocationUpdateOtherUsesView.as_view(), name='update--location--other--uses'),
-  path('location/<str:pk>/update/other/tenures', location__other__tenures.LocationUpdateOtherTenuresView.as_view(), name='update--location--other--tenures'),
-  path('location/<str:pk>/update/other/approach', location__other__approach.LocationUpdateOtherApproachView.as_view(), name='update--location--other--approach'),
+  path('location/<str:pk>/update/geo-info--admin-areas', location__geo_info__admin_areas.LocationUpdateGeoInfoAdminAreasView.as_view(), name='update--location--geo-info--admin-areas'),
+  path('location/<str:pk>/update/geo-info--land-parcels', location__geo_info__land_parcels.LocationUpdateGeoInfoLandParcelsView.as_view(), name='update--location--geo-info--land-parcels'),
+  path('location/<str:pk>/update/other--relations', location__other__relations.LocationUpdateOtherRelationsView.as_view(), name='update--location--other--relations'),
+  path('location/<str:pk>/update/other--uses', location__other__uses.LocationUpdateOtherUsesView.as_view(), name='update--location--other--uses'),
+  path('location/<str:pk>/update/other--tenures', location__other__tenures.LocationUpdateOtherTenuresView.as_view(), name='update--location--other--tenures'),
+  path('location/<str:pk>/update/other--approach', location__other__approach.LocationUpdateOtherApproachView.as_view(), name='update--location--other--approach'),
 
   ## Location features ##
   # Full details view, normally loaded into main page via AJAX when a node selected.
